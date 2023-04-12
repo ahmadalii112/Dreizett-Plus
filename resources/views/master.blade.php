ee6faa14f28eaba69edf34732521e37f35f5849a
viacoding1 <32134634+viacoding1@users.noreply.github.com>
2023-04-12 17:34:36 +0200

    force SSL with htaccess and added google tagmanager

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,9 +7,21 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <title>dreizett Plus - Ambulanter Pflegedienst</title>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5646Z85');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5646Z85"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <header class="bg-white">
         <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div class="flex lg:flex-1">
